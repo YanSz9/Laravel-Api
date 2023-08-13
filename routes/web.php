4e27a8a25e73d9aca4ebd136105b43c331b3d841
Yan Souza <yansouza009@gmail.com>
2023-08-13 15:43:39 -0300

    Discovering things

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/empresa', function () {
+    return view('site/empresa');
+});
+
+Route::any('/any', function () {
+    return "Permite todo tipo de acesso http";
+});
+
+Route::match(['get', 'post'], '/match', function () {
+    return "Permite apenas acessos definidos";
+});
+
+Route::get('/produto/{id}/{cat}', function ($id, $cat) {
+    return "O id do produto é: " . $id . "<br>" . "A categoria é:" . $cat;
+});
+
+Route::redirect('/sobre', "/empresa");
+Route::view("/empresa", "site/empresa");
+
+Route::get("/news", function () {
+    return view("news");
+})->name("notícias");
+
+Route::get("/novidades", function () {
+    return redirect()->route("notícias");
+});
